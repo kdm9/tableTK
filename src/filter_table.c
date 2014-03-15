c7cb84d4b5d5429acb9a3e7b873c68b31daae6d5
Kevin Murray <spam@kdmurray.id.au>
2014-03-15 14:45:23 +1100

    add v0.0a source

---
+++ src/filter_table.c
@@ -0,0 +1,219 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  filterTable main script
+ *
+ *        Version:  1.0
+ *        Created:  14/03/14 14:11:29
+ *       Revision:  none
+ *        License:  GPLv3+
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdint.h>
+
+
+#include "filter_table.h"
+
+int ft_median (FT_CELL_TYPE *data, FT_CELL_TYPE threshold, size_t count)
+{
+    FT_CELL_TYPE med = quick_select(data, count);
+    return med >= (FT_CELL_TYPE) threshold;
+}
+
+int ft_num_nonzero (FT_CELL_TYPE *data, FT_CELL_TYPE threshold, size_t count)
+{
+    size_t iii = 0;
+    size_t passes = 0;
+    for (iii; iii<count; ){
+        if (data[iii++] > 0.) passes++;
+    }
+    return passes >= threshold;
+}
+
+int filter_table (table_t *tab)
+{
+    char *buf = calloc(FT_BUFFSIZE, sizeof(*buf));
+    FT_CELL_TYPE *cel_buf = calloc(FT_BUFFSIZE, sizeof(*cel_buf));
+    size_t row = 0;
+    while(fgets(buf, FT_BUFFSIZE, tab->fp) != NULL) {
+        if (row < tab->skiprow) {
+            row++;
+            fprintf(tab->outfp, "%s", buf);
+            continue;
+        }
+        size_t col = 0;
+        size_t cell = 0;
+        char *tok_tmp = NULL;
+        char *token = NULL;
+        char *tok_line = strdup(buf);
+        token = strtok_r(tok_line, tab->sep, &tok_tmp);
+        while (token != NULL) {
+            if (col < tab->skipcol) {
+                col++;
+                continue;
+            }
+            cel_buf[cell++] = atof(token);
+            col++;
+            token = strtok_r(NULL, tab->sep, &tok_tmp);
+        }
+        row++;
+        if ((*(tab->filter_fn))(cel_buf, tab->threshold, col + 1)) {
+            fprintf(tab->outfp, "%s", buf);
+        }
+        memset(buf, 0, FT_BUFFSIZE);
+        memset(cel_buf, 0, FT_BUFFSIZE);
+        free(tok_line);
+    }
+    free(buf);
+    free(cel_buf);
+    return 1;
+}
+
+void print_usage()
+{
+    fprintf(stderr, "filterTable\n\n");
+    fprintf(stderr, "Filter a large table row-wise.\n\n");
+    fprintf(stderr, "USAGE:\n\n");
+    fprintf(stderr, "filterTable [-r ROWS -c COLS -i INFILE -o OUTFILE -s SEP] -m | -z THRESH\n\n");
+    fprintf(stderr, "OPTIONS:\n");
+    fprintf(stderr, "\t-m THRESH\tUse median method of filtering, with threshold THRESH.\n");
+    fprintf(stderr, "\t-z THRESH\tUse number of non-zero cells to filter, with threshold THRESH.\n");
+    fprintf(stderr, "\t-r ROWS\t\tSkip ROWS rows from start of table.\n");
+    fprintf(stderr, "\t-c COLS\t\tSkip COLS columns from start of each row.\n");
+    fprintf(stderr, "\t-s SEP\t\tUse string SEP as field seperator, not \"\\t\".\n");
+    fprintf(stderr, "\t-i INFILE\tInput from INFILE, not stdin (or '-' for stdin).\n");
+    fprintf(stderr, "\t-o OUTFILE\tOutput to OUTFILE, not stdout (or '-' for stdout).\n");
+}
+
+int parse_args (int argc, char *argv[], table_t *tab)
+{
+    unsigned char haveflags = 0;
+    /*
+        1 1 1 1 1 1 1 1
+            | | | | | \- method
+            | | | | \--- out fname
+            | | | \----- in fname
+            | | \------- cols to skip
+            | \--------- rows to skip
+            \----------- Field sep
+    */
+    char c = '\0';
+    while((c = getopt(argc, argv, "m:z:r:c:o:i:s:")) >= 0) {
+        switch (c) {
+            case 'm':
+                haveflags |= 1;
+                tab->filter_fn = &ft_median;
+                tab->threshold = atof(optarg);
+                break;
+            case 'z':
+                haveflags |= 1;
+                tab->filter_fn = &ft_num_nonzero;
+                tab->threshold = atof(optarg);
+                break;
+            case 'o':
+                haveflags |= 2;
+                tab->outfname = strdup(optarg);
+                break;
+            case 'i':
+                haveflags |= 4;
+                tab->fname = strdup(optarg);
+                break;
+            case 'c':
+                haveflags |= 8;
+                tab->skipcol = atol(optarg);
+                break;
+            case 'r':
+                haveflags |= 16;
+                tab->skiprow = atol(optarg);
+                break;
+            case 's':
+                haveflags |= 32;
+                tab->sep = strdup(optarg);
+                break;
+            /* case '?':
+                fprintf(stderr, "[parse_args] Bad argument '-%c'\n", optopt);
+                print_usage();
+                return 0;*/
+        }
+    }
+    if (tab->sep == NULL) {
+        tab->sep = strdup("\t");
+    }
+    /* Setup input fp */
+    if ((!haveflags & 4) || tab->fname == NULL || \
+            strncmp(tab->fname, "-", 1) == 0) {
+        tab->fp = fdopen(fileno(stdout), "r");
+        tab->fname = strdup("stdin");
+        haveflags |= 4;
+    } else {
+        tab->fp = fopen(tab->fname, "r");
+    }
+    if (tab->fp == NULL) {
+        fprintf(stderr, "Could not open file '%s'\n%s\n", tab->fname,
+                strerror(errno));
+        return 0;
+    }
+    /* Setup output fp */
+    if ((!haveflags & 2) || tab->outfname == NULL || \
+            strncmp(tab->outfname, "-", 1) == 0) {
+        tab->outfp = fdopen(fileno(stdout), "w");
+        tab->outfname = strdup("stdin");
+        haveflags |= 2;
+    } else {
+        tab->outfp = fopen(tab->outfname, "w");
+    }
+    if (tab->outfp == NULL) {
+        fprintf(stderr, "Could not open file '%s'\n%s\n", tab->outfname,
+                strerror(errno));
+        return 0;
+    }
+    if ((haveflags & 7) != 7) {
+        fprintf(stderr, "[parse_args] Required arguments missing\n");
+        print_usage();
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+
+/*
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ * =====================================================================================
+ */
+int
+main (int argc, char *argv[])
+{
+    if (argc == 1) {
+        print_usage();
+        exit(EXIT_SUCCESS);
+    }
+    table_t *tab = calloc(1, sizeof(*tab));
+    if (tab == NULL) {
+        fprintf(stderr, "Cannot allocate memory.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!parse_args(argc, argv, tab)) {
+        destroy_table_t(tab);
+        fprintf(stderr, "Cannot parse arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!filter_table(tab)) {
+        destroy_table_t(tab);
+        fprintf(stderr, "Error during table filtering.\n");
+        exit(EXIT_FAILURE);
+    }
+    destroy_table_t(tab);
+    return EXIT_SUCCESS;
+} /* ----------  end of function main  ---------- */
---
+++ src/filter_table.h
@@ -0,0 +1,117 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  filter_table.h
+ *
+ *    Description:  filter_table header
+ *
+ *        Version:  1.0
+ *        Created:  14/03/14 14:49:07
+ *       Revision:  none
+ *        License:  GPLv3+
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#ifndef FILTER_TABLE_H
+#define FILTER_TABLE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
+#define	FT_CELL_TYPE double
+
+#define	FT_BUFFSIZE 1<<11
+
+typedef struct _table {
+    FILE *fp;
+    char *fname;
+    FILE *outfp;
+    char *outfname;
+    int64_t skiprow;
+    int64_t skipcol;
+    FT_CELL_TYPE threshold;
+    char *sep;
+    int (*filter_fn)(FT_CELL_TYPE *, FT_CELL_TYPE, size_t);
+} table_t;
+
+
+#define	destroy_table_t(t) do {                                             \
+    if ((t) != NULL) {                                                      \
+        if ((t)->fname != NULL) free((t)->fname);                           \
+        if ((t)->outfname != NULL) free((t)->outfname);                        \
+        if ((t)->sep != NULL) free((t)->sep);                               \
+        if ((t)->fp != NULL) fclose((t)->fp);                               \
+        if ((t)->outfp != NULL) fclose((t)->outfp);                         \
+        free((t));                                                          \
+    }} while (0)
+
+/*
+ *  This Quickselect routine is based on the algorithm described in
+ *  "Numerical recipes in C", Second Edition,
+ *  Cambridge University Press, 1992, Section 8.5, ISBN 0-521-43108-5
+ *  This code by Nicolas Devillard - 1998. Public domain.
+ */
+
+#define ELEM_SWAP(a,b) { register FT_CELL_TYPE t=(a);(a)=(b);(b)=t; }
+
+
+static inline FT_CELL_TYPE quick_select(FT_CELL_TYPE arr[], int n)
+{
+    int low, high;
+    int median;
+    int middle, ll, hh;
+
+    low = 0 ; high = n-1 ; median = (low + high) / 2;
+    for (;;) {
+        if (high <= low) /* One element only */
+            return arr[median] ;
+
+        if (high == low + 1) {  /* Two elements only */
+            if (arr[low] > arr[high])
+                ELEM_SWAP(arr[low], arr[high]) ;
+            return arr[median] ;
+        }
+
+    /* Find median of low, middle and high items; swap into position low */
+    middle = (low + high) / 2;
+    if (arr[middle] > arr[high])    ELEM_SWAP(arr[middle], arr[high]) ;
+    if (arr[low] > arr[high])       ELEM_SWAP(arr[low], arr[high]) ;
+    if (arr[middle] > arr[low])     ELEM_SWAP(arr[middle], arr[low]) ;
+
+    /* Swap low item (now in position middle) into position (low+1) */
+    ELEM_SWAP(arr[middle], arr[low+1]) ;
+
+    /* Nibble from each end towards middle, swapping items when stuck */
+    ll = low + 1;
+    hh = high;
+    for (;;) {
+        do ll++; while (arr[low] > arr[ll]) ;
+        do hh--; while (arr[hh]  > arr[low]) ;
+
+        if (hh < ll)
+        break;
+
+        ELEM_SWAP(arr[ll], arr[hh]) ;
+    }
+
+    /* Swap middle item (in position low) back into correct position */
+    ELEM_SWAP(arr[low], arr[hh]) ;
+
+    /* Re-set active partition */
+    if (hh <= median)
+        low = ll;
+        if (hh >= median)
+        high = hh - 1;
+    }
+}
+
+#undef ELEM_SWAP
+
+#endif /* FILTER_TABLE_H */
